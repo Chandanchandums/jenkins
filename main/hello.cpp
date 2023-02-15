6f23b17ec5c16d3d50ed3b45ead085993f28cb7e
Chandanchandums <chandanms723@gmail.com>
2023-02-15 19:26:51 +0530

    jenkins_lab

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Chandan here PES2UG20CS094\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run \n";
+  return 0;
+}
