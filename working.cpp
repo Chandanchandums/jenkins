11fceddf478847ade61e53e6ce9d8ae8a5a92346
Chandanchandums <93769506+Chandanchandums@users.noreply.github.com>
2023-02-15 23:21:33 +0530

    Update working.cpp

--- working.cpp
+++ working.cpp
@@ -1,9 +1,33 @@
-#include <iostream>
-using namespace std;
-
-int main(void) {
-  cout << "Hello, Chandan here PES2UG20CS094\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run i have modified it \n";
-  return 0;
+pipeline{
+    agent any
+    stages{
+        stage('Build'){
+            steps{
+                sh 'mvn clean install'
+                echo 'Build Stage successful'
+            }
+        }
+        stage('Test'){
+            steps{
+                sh 'mvn test'
+                echo 'Test Stage Successful'
+                post {
+                    always{
+                        junit 'target/surefire-reports/*.xml'
+                    }
+                }
+            }
+        }
+        stage('Deploy'){
+            steps{
+                sh 'mvn deploy'
+                echo 'Deployment Successful'
+            }
+        }
+    }
+    post {
+        failure{
+            echo 'pipeline failed'
+        }
+    }
 }
